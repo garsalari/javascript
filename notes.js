53e567d9c70c91c542a1747ef04c7a82ccf42cc5
garsalari <71303696+garsalari@users.noreply.github.com>
2024-05-22 21:42:17 -0400

    Create notes.js

---
+++ notes.js
@@ -0,0 +1,26 @@
+console.log('--------------------------------------');
+// const vs let
+const contador = 'contador';
+console.log(contador);
+let contadorlet = 'contadorlet';
+console.log(contadorlet);
+
+contadorlet = 5;
+console.log(contadorlet);
+
+// ambos nos permiten ASIGNAR valores
+// const no permite REASIGNAR valores
+// let permite REASIGNAR valores incluso cambiando el type
+// const necesita un valor 
+console.log('--------------------------------------');
+// ambito
+
+const amb = 'amb';
+console.log(amb);
+if (true) {
+  const amb = 'bma';
+  console.log(amb);
+}
+console.log(amb);
+// let y const no permiten duplicar una variable en el mismo ambito
+// JS busca en el ambito y luego hacia arriba
