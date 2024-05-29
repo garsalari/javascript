7df764007cf40cb91a0024feccc09f432cef5657
garsalari <71303696+garsalari@users.noreply.github.com>
2024-05-29 14:16:37 -0400

    Create java.java

---
+++ java.java
@@ -0,0 +1,51 @@
+/*
+----------------------------
+  Features
+  ---open source
+  ---high performance
+  ---multithreaded
+  ---secured
+  ---platform independent
+  ---portability
+  ---object oriented
+  ---bobust
+
+------------------------------
+    Variable
+    variable is a container that holds the value while java program is being executed
+
+    3 Types od variable
+    *local     => variable declared inside the body of a method is called local variable you can use it only within that method [local can not be defined with static]
+    *instance  => variable declared inside the class but outside the body of the method 
+    *static    => it means that the variable belongs to the class itself rather than to any specific instance of the class
+------------------------------
+    datatypes
+    int      2 Bytes
+    float    4
+    double   8
+    long     8
+    char     2
+    short    1
+    byte     1
+    boolean  1
+---------------------------------
+    operators in java
+    Unary operator        => are the types that need only one operand to perform any operation like increment, decrement, negation, etc //
+      // Main driver method
+    public static void main(String[] args)
+    {
+        // Declaring a custom variable
+        int n1 = 20;
+ 
+        // Printing the above variable
+        System.out.println("Number = " + n1);  // 20
+ 
+        // Performing unary operation
+        n1 = -n1;
+ 
+        // Printing the above result number
+        // after unary operation
+        System.out.println("Result = " + n1);  // -20
+    }
+    
+  */
