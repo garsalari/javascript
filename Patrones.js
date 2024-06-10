1b1cf5000b4b413360fa59bc13562d4da6a2d4ee
garsalari <71303696+garsalari@users.noreply.github.com>
2024-06-09 21:54:52 -0400

    Create Patrones.js

---
+++ Patrones.js
@@ -0,0 +1,13 @@
+patrones
+
+Antipatrones de desarrollo
+The Blob (“Clases gigantes”): Este antipatrón se da en objetos con muchos atributos o muchas operaciones. Esto rompe las ventajas de la programación OO, 
+    ya que estas clases tan grandes son muy difíciles de mantener, de reusar y de probar. Suele aparecer por un diseño malo o debido a sistemas heredados.
+Lava Flow (“Código muerto”): Este antipatrón se da cuando se entrega software antes de ser terminado o suficientemente probado que tiene un código no óptimo y al ser expuesto,
+    sus características no pueden ser modificadas.
+Poltergeists (“No se sabe bien lo que hacen algunas clases”): Esta mala práctica se refiere a objetos de un ciclo de vida corto cuya única función suele ser invocar métodos de otros objetos.
+    Esto hace que crezca la dificultad para mantener el sistema.
+Golden Hammer (“Para un martillo todo son clavos”): Este antipatrón se refiere al uso de una tecnología, patrón, arquitectura etc..
+    para cualquier problema o situación incluso cuando es evidente que no va ser útil.
+Spaghetti Code (“Muchos if ó switch”): se refiere a un código mal estructurado.
+Cut & Paste programming (“Cortar y pegar código”).
